6f5b51b19d4183b8e2c2cc56fbab362d3d0bead6
steven nguyen <nguyeste008@students.garlandisd.net>
2021-09-29 08:08:40 -0500

    update tests

--- index.test.ts
+++ index.test.ts
@@ -50,46 +50,60 @@ describe('System', () => {
 
    test('If A vs B and A wins, A.rating > B.rating', () => {
       const game1 = new Game([playerA, playerB], undefined, true)
-      game1.finish([1, 0])
+      game1.finish({
+         [playerA.id]: 1,
+         [playerB.id]: 0,
+      })
 
       expect(playerA.rating.value).toBeGreaterThan(playerB.rating.value)
    })
 
    test('After A wins once, if B wins twice, B.rating > A.rating', () => {
       const game2 = new Game([playerA, playerB], undefined, true)
-      game2.finish([0, 1])
+      game2.finish({
+         [playerA.id]: 0,
+         [playerB.id]: 1,
+      })
 
       const game3 = new Game([playerA, playerB], undefined, true)
-      game3.finish([0, 1])
+      game3.finish({
+         [playerA.id]: 0,
+         [playerB.id]: 1,
+      })
 
       expect(playerB.rating.value).toBeGreaterThan(playerA.rating.value)
    })
 
    test('After ABBA, A.rating === B.rating', () => {
       const game4 = new Game([playerA, playerB], undefined, true)
-      game4.finish([1, 0])
+      game4.finish({
+         [playerA.id]: 1,
+         [playerB.id]: 0,
+      }))
 
       expect(playerA.rating.value).toBeCloseTo(playerB.rating.value)
    })
 
-   const random = new Bot(null, new Version(1, 0, 0))
-   const plusPtOne = new Bot(null, new Version(1, 0, 0))
-   const plusPtTwo = new Bot(null, new Version(1, 0, 0))
+   const random = new Bot(false, null, new Version(1, 0, 0))
+   const plusPtOne = new Bot(false, null, new Version(1, 0, 0))
+   const plusPtTwo = new Bot(false, null, new Version(1, 0, 0))
 
    test('random < plusPtOne < plusPtTwo', () => {
       for (let i = 0; i < 100; i++) {
          const game5thru104 = new Game([random, plusPtOne, plusPtTwo], undefined, true)
          const scores = [Math.random(), Math.random() + 0.1, Math.random() + 0.2] as const
          const total = scores[0] + scores[1] + scores[2]
-         // @ts-expect-error seriously?
-         game5thru104.finish(scores.map(score => score / total) as readonly [number, number, number])
+         game5thru104.finish({
+            [random.id]: scores[0] / total,
+            [plusPtOne.id]: scores[1] / total,
+            [plusPtTwo.id]: scores[2] / total,
+         })
       }
 
       console.info({
          random: random.rating.value,
          plusPtOne: plusPtOne.rating.value,
          plusPtTwo: plusPtTwo.rating.value,
-         certainty: random.rating.certaintyAgainstPlayers([plusPtOne, plusPtTwo])
       })
 
       expect(random.rating.value).toBeLessThan(plusPtOne.rating.value)
